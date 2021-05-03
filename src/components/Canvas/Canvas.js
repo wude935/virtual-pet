62e4a851564b800fbfdff601739a3f9d8eaffee1
Caitlin <caitlin.zhu2010@gmail.com>
2021-05-03 17:43:22 -0500

    attempt_loadImage

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -2,37 +2,52 @@
 import './Canvas.css';
 import { useCallback } from "react";
 import Sketch from "react-p5";
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
 
 let a = 300;
 let b = 300;
 let speed = 3;
 
 function Canvas(props) {
+    var numFrames = 11;
+    let img;
+    function preload(p5) {
+        img = p5.loadImage('./standing.png');
+      }
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(canvasParentRef.offsetWidth, canvasParentRef.offsetHeight).parent(canvasParentRef);
+        var i;
+        // for (i = 0; i < x_sprite.length; i++){
+        //     var imgName = "x_running-" + p5.nf(i+1, 2) + ".gif";
+        //     x_sprite.push(p5.loadImage(imgName));
+        //   }
+        console.log(img);
     };
 
-
     const draw = useCallback((p5, canvasParentRef) => {
-        if (props.game.start) {
-            if (props.pet.boredom > 10) {
-                p5.background("rgb(0%,100%,10%)");
-            }
-            else {
-                p5.background("rgb(100%,10%,10%)");
-            }
-            p5.stroke(255);
-            p5.strokeWeight(4);
-            p5.noFill();
-            p5.rect(a, b, 100, 200);
-            if (a >= p5.width) {
-                speed = -3;
-            }
-            if (a === 0) {
-                speed = 3;
-            }
-            a = a + speed;
-        }
+        var frame = p5.frameCount % numFrames;
+        p5.image(img, 250, 250);
+        // p5.image(x_sprite[frame], 250, 250);
+        // if (props.game.start) {
+        //     if (props.pet.boredom > 10) {
+        //         p5.background("rgb(100%,100%,100%)");
+        //     }
+        //     else {
+        //         p5.background("rgb(100%,10%,10%)");
+        //     }
+        //     p5.stroke(255);
+        //     p5.strokeWeight(4);
+        //     p5.noFill();
+        //     p5.rect(a, b, 100, 200);
+        //     if (a >= p5.width) {
+        //         speed = -3;
+        //     }
+        //     if (a === 0) {
+        //         speed = 3;
+        //     }
+        //     a = a + speed;
+        // }
     }, [props.pet, props.game]);
 
     return (
---
+++ src/components/Canvas/Canvas.tsx
@@ -0,0 +1,34 @@
+import Sketch from "react-p5";
+
+function Canvas(props: any) {
+    // let a = 300;
+    // let b = 300;
+    // let speed = 3;
+
+    function setup(p5: any, canvasParentRef: any) {
+        let xyz = p5.createCanvas(1000, 800).parent(canvasParentRef);
+        //Canvas of size 1000x800 
+    };
+    
+    function draw(p5: any) {
+        // p5.background("rgb(100%,100%,100%)");
+        
+        // p5.stroke(255);
+        // p5.strokeWeight(4);
+        // p5.noFill();
+        // p5.rect(a, b, 100, props.rectHeight);
+        // if (a >= p5.width) {
+        //     speed = -3;
+        // }
+        // if (a === 90) {
+        //     speed = 3;
+        // }
+        // a = a + speed;  
+    };
+    
+    return (
+        <Sketch setup={setup} draw={draw} style={{height: "100%", width: "100%"}} />          
+    );
+}
+
+export default Canvas;
