8c082e6ddbda801f07bc93e5f684d685f057b2c0
Caitlin <caitlin.zhu2010@gmail.com>
2021-05-10 19:42:57 -0500

    kind of buttons work now idk WIP

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -10,15 +10,23 @@ import img3 from "../../images/run.gif";
 import img4 from "../../images/food1.png";
 import img5 from "../../images/food2.png";
 import img6 from "../../images/ded.png";
+import img7 from "../../images/ball.png";
+import img8 from "../../images/bath.gif";
+import img9 from "../../images/eat.gif";
 
 let x;
 let y;
+// idle animations
 let stand;
 let bored;
 let run;
 let food1;
 let food2;
+let eat;
 let ded;
+let ball;
+let bath;
+let tempy;
 function Canvas(props) {
     const {game, setGame} = useContext(SetGameContext);
     const {pet, setPet, petActions} = useContext(SetPetContext);
@@ -31,14 +39,23 @@ function Canvas(props) {
         food1 = p5.loadImage(img4);
         food2 = p5.loadImage(img5);
         ded = p5.loadImage(img6);
+        ball = p5.loadImage(img7);
+        bath = p5.loadImage(img8);
+        eat = p5.loadImage(img9);
         console.log(stand)
         console.log(bored)
         console.log(run)
         console.log(food1)
         console.log(food2)
         console.log(ded)
+        console.log(ball)
+        console.log(bath)
+        console.log(eat)
+
+        // location of pet at all times
         x = p5.width/2-100;
-        y = 0;
+        y = p5.height-300;
+        tempy = 0;
     };
 
     const draw = useCallback((p5, canvasParentRef) => {
@@ -46,39 +63,65 @@ function Canvas(props) {
         if (game.start) {
             //HERE IS THE CODE CAITLIN
             // go to setpetcontext line 57 and change the time it takes the animation to run
-            if (pet.action == 'feed'){
+            // feed
+            if (pet.action == "feed"){
                 console.log('run animation')
-            }
-
-            // bored
-            if (pet.boredom <= 70) {
-                p5.background("rgb(100%,10%,10%)");
-                p5.image(run, x, p5.height-300);
-                x -= 3;
-                run.resize(300, 0);
-            }
-            else {
-                p5.image(stand, x, p5.height-300);
-                stand.resize(300, 0);
-            }
-            
-            // hungry
-            if (pet.hunger <= 60){
                 food1.resize(300,0);
-                if (y != p5.height-300){
-                    p5.image(food1, 400, y);
-                    y += 10;
+                eat.resize(300, 0);
+                // food drops
+                if (tempy != p5.height){
+                    p5.image(food1, 400, tempy);
+                    tempy += 15;
                 }
                 else{
-                    p5.image(food2, 400, p5.height-300)
+                    p5.background("rgb(100%, 100%, 100%)");
                     food2.resize(300, 0);
+                    p5.image(food2, 400, p5.height-300)
+
+                    // runs to food and eats
+                    if (x < 350){
+                        p5.image(run, x, y);
+                        x += 5;
+                    }
+                    else if (x>450){
+                        p5.image(run, x, y);
+                        x -= 5;
+                    }
+                    else {
+                        p5.background("rgb(100%, 100%, 100%)");
+                        p5.image(eat, x, y);
+                    }
                 }
             }
 
-            // ded
+            // play
+            if (pet.action == "play"){
+                p5.background("rgb(100%, 100%, 100%)");
+                ball.resize(200,0);
+                run.resize(300, 0);
+                p5.image(ball, 100, p5.height-200);
+                if (x < 50){
+                    p5.image(run, x, y);
+                    x += 5;
+                }
+                else if (x>150){
+                    p5.image(run, x, y);
+                    x -= 5;
+                }
+                else {
+                    p5.image(stand, x, y);
+                }
+            }
+            
+            // clean
+            if (pet.action == "clean"){
+                p5.background("rgb(100%, 100%, 100%)");
+                bath.resize(300, 0);
+                p5.image(bath, x, y);
+            }
             if (!pet.alive){
                 p5.background("rgb(100%, 100%, 100%)");
-                p5.image(ded, x, p5.height-300);
+                p5.image(ded, x, y);
                 ded.resize(300, 0);
             }
         }
